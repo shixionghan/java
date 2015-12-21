d9f30d848e45cb60d74d0d44bb0fd9b97a277551
Admin <Admin@Admin-PC>
2015-12-21 10:18:37 +0800

    test

---
+++ src/main/java/com/camlot/test/Test.java
@@ -0,0 +1,44 @@
+package com.camlot.test;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Test {
+
+
+		
+		/**
+		 * 
+		 * <p>Discription:MD5加密</p>
+		 * Created on 2015年2月11日
+		 * @param str
+		 * @return
+		 * @author:胡恒心
+		 */
+		public static String encipher(String str) {
+	        MessageDigest messageDigest = null;  
+	        try {  
+	            messageDigest = MessageDigest.getInstance("MD5");  
+	  
+	            messageDigest.reset();  
+	  
+	            messageDigest.update(str.getBytes("UTF-8"));  
+	        } catch (NoSuchAlgorithmException e) {
+	        } catch (UnsupportedEncodingException e) {
+	        }  
+	  
+	        byte[] byteArray = messageDigest.digest();  
+	  
+	        StringBuffer md5StrBuff = new StringBuffer();  
+	  
+	        for (int i = 0; i < byteArray.length; i++) {              
+	            if (Integer.toHexString(0xFF & byteArray[i]).length() == 1)  
+	                md5StrBuff.append("0").append(Integer.toHexString(0xFF & byteArray[i]));  
+	            else  
+	                md5StrBuff.append(Integer.toHexString(0xFF & byteArray[i]));  
+	        }  
+	  
+	        return md5StrBuff.toString();  
+	    }  
+
+}
