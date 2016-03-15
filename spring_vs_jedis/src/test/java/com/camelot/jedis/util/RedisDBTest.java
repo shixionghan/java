9fe37cc6472e5dec9e5442b2e253f8b3bd89bdfa
hanshixiong <shixiong_han@163.com>
2016-03-15 17:59:49 +0800

    发斯蒂芬

--- spring_vs_jedis/src/test/java/com/camelot/jedis/util/RedisDBTest.java
+++ spring_vs_jedis/src/test/java/com/camelot/jedis/util/RedisDBTest.java
@@ -2,7 +2,6 @@ package com.camelot.jedis.util;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
